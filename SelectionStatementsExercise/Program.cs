a80a066d09a576d0452c3120f652dcd4c7595970
tmoore28 <mooretylor28@gmail.com>
2021-11-28 20:34:36 -0500

    else if statments first attempt

---
+++ SelectionStatementsExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try to guess my favorite number!");
+            int favNumber = 7;
+            string input = Console.ReadLine();
+            int userGuess = int.Parse(input);
+                if (userGuess < favNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+                else if (userGuess > favNumber)
+            {
+                Console.WriteLine("Too high");
+            }
+                else
+            {
+                Console.WriteLine("Nevermind");
+            }
+        }
+    }
+}
